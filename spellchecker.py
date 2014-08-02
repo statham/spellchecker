de4beeb9e7e5f114d933b9484a3f6988011505c0
statham <kstatham@mit.edu>
2014-08-02 18:54:05 -0400

    skeleton for methods

---
+++ spellchecker.py
@@ -0,0 +1,21 @@
+def readDict():
+  #get file from /usr/share/dict/words
+  #return list of words 
+  pass
+
+def spellcheck(input, words):
+  #if word is correct, just return word
+
+  #if incorrect
+    #tolowercase
+    #repeated letters
+    #incorrect vowels
+
+  pass
+
+def run():
+  #get dictionary
+  #words = readDict()
+
+  #take input and return suggestion until killed
+  pass
