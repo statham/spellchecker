d7b525471fdb518cd3e33ba889a73ea39664c9bb
statham <kstatham@mit.edu>
2014-08-02 21:15:09 -0400

    basic implementation of spellchecker method

--- spellchecker.py
+++ spellchecker.py
@@ -9,14 +9,37 @@ def readDict():
   return words
 
 def spellcheck(input, words, wordsHash):
-  #if word is correct, just return word
+  if input not in wordsHash:
 
-  #if incorrect
-    #tolowercase
-    #repeated letters
-    #incorrect vowels
+    	#tolowercase
+	lowercase = input.lower()
+	for word in words:
+		if (input == word) or (lowercase == word):
+			wordsHash[input] = word
+			#stop and return
 
-  pass
+	#repeated letters
+	for letter, g in itertools.groupby(input):
+		group = list(g)
+		if len(group) > 1:
+			rep_input = input.replace("".join(group), letter)
+			for word in words:
+				if rep_input == word:
+					wordsHash[input] = word
+					#stop and return
+
+	#incorrect vowels
+	vowels = 'aeiou'
+	for letter in vowels_input:
+		if letter in vowels:
+			for vowel in vowels:
+				vow_input = input.replace(letter, vowel)
+				for word in words:
+					if vow_input == word:
+						wordsHash[input] = word
+						#stop and return
+			
+  return wordsHash[input]
 
 def run():
   #get dictionary
